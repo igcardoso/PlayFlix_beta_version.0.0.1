5f87aabcec1019dbd4a0b5743289cbcc10b12710
igor <85964276+igcardoso@users.noreply.github.com>
2022-03-31 10:31:00 -0300

    Introduction of hover car highlight shift

--- css/PlayFlix_beta_version.0.0.1/js/app.js
+++ css/PlayFlix_beta_version.0.0.1/js/app.js
@@ -66,3 +66,105 @@ $(document).ready(() => {
     }
   });
 });
+
+
+$(document).ready(function() {
+  $('#movie01').hover(function() {
+    $('.hero-slide-item1').css({
+      background: 'url(images/movies/theatre-dead.jpg), no-repeat center', backgroundSize: 'cover'
+    });
+    $('.item-content-title1').text('Theatre of the Dead');
+    $('.star1 span').text('6.1');
+    $('.time1 span').text('2h 29m');
+    $('.class1 span').text('12+');
+    $('.item-content-description1').text('Optimus Prime encontra seu planeta natal, Cybertron, agora um planeta morto, e descobre que foi responsável por matá-lo. Ele encontra uma maneira de trazer o planeta de volta à vida, mas para isso precisa encontrar um artefato que está na Terra.');
+    $('.item-action1').css({
+      display: 'none'
+    });
+
+  });
+  $('#movie02').hover(function() {
+    $('.hero-slide-item1').css({
+      background: 'url(https://www.themoviedb.org/t/p/original/8yLzd8z7xRQzWG0neDrVLc34Jmx.jpg), no-repeat center', backgroundSize: 'cover'
+    });
+    $('.item-content-title1').text('Transformes');
+    $('.star1 span').text('6.1');
+    $('.time1 span').text('2h 29m');
+    $('.class1 span').text('12+');
+    $('.item-content-description1').text('Optimus Prime encontra seu planeta natal, Cybertron, agora um planeta morto, e descobre que foi responsável por matá-lo. Ele encontra uma maneira de trazer o planeta de volta à vida, mas para isso precisa encontrar um artefato que está na Terra.');
+    $('.item-action1').css({
+      display: 'none'
+    });
+
+  });
+  $('#movie03').hover(function() {
+    $('.hero-slide-item1').css({
+      background: 'url(images/movies/resident-evil.jpg), no-repeat center', backgroundSize: 'cover'
+    });
+    $('.item-content-title1').text('Resident Evil');
+    $('.star1 span').text('6.1');
+    $('.time1 span').text('2h 29m');
+    $('.class1 span').text('12+');
+    $('.item-content-description1').text('Optimus Prime encontra seu planeta natal, Cybertron, agora um planeta morto, e descobre que foi responsável por matá-lo. Ele encontra uma maneira de trazer o planeta de volta à vida, mas para isso precisa encontrar um artefato que está na Terra.');
+    $('.item-action1').css({
+      display: 'none'
+    });
+
+  });
+  $('#movie04').hover(function() {
+    $('.hero-slide-item1').css({
+      background: 'url(images/movies/captain-marvel.png), no-repeat center', backgroundSize: 'cover'
+    });
+    $('.item-content-title1').text('Captan Marvel');
+    $('.star1 span').text('6.1');
+    $('.time1 span').text('2h 29m');
+    $('.class1 span').text('12+');
+    $('.item-content-description1').text('Optimus Prime encontra seu planeta natal, Cybertron, agora um planeta morto, e descobre que foi responsável por matá-lo. Ele encontra uma maneira de trazer o planeta de volta à vida, mas para isso precisa encontrar um artefato que está na Terra.');
+    $('.item-action1').css({
+      display: 'none'
+    });
+
+  });
+  $('#movie05').hover(function() {
+    $('.hero-slide-item1').css({
+      background: 'url(images/movies/hunter-killer.jpg), no-repeat center', backgroundSize: 'cover'
+    });
+    $('.item-content-title1').text('Hunter Killer');
+    $('.star1 span').text('6.1');
+    $('.time1 span').text('2h 29m');
+    $('.class1 span').text('12+');
+    $('.item-content-description1').text('Optimus Prime encontra seu planeta natal, Cybertron, agora um planeta morto, e descobre que foi responsável por matá-lo. Ele encontra uma maneira de trazer o planeta de volta à vida, mas para isso precisa encontrar um artefato que está na Terra.');
+    $('.item-action1').css({
+      display: 'none'
+    });
+
+  });
+  $('#movie06').hover(function() {
+    $('.hero-slide-item1').css({
+      background: 'url(images/movies/blood-shot.jpg), no-repeat center', backgroundSize: 'cover'
+    });
+    $('.item-content-title1').text('Bloodshot');
+    $('.star1 span').text('6.1');
+    $('.time1 span').text('2h 29m');
+    $('.class1 span').text('12+');
+    $('.item-content-description1').text('Optimus Prime encontra seu planeta natal, Cybertron, agora um planeta morto, e descobre que foi responsável por matá-lo. Ele encontra uma maneira de trazer o planeta de volta à vida, mas para isso precisa encontrar um artefato que está na Terra.');
+    $('.item-action1').css({
+      display: 'none'
+    });
+
+  });
+  $('#movie07').hover(function() {
+    $('.hero-slide-item1').css({
+      background: 'url(images/movies/call.jpg), no-repeat center', backgroundSize: 'cover'
+    });
+    $('.item-content-title1').text('Call');
+    $('.star1 span').text('6.1');
+    $('.time1 span').text('2h 29m');
+    $('.class1 span').text('12+');
+    $('.item-content-description1').text('Optimus Prime encontra seu planeta natal, Cybertron, agora um planeta morto, e descobre que foi responsável por matá-lo. Ele encontra uma maneira de trazer o planeta de volta à vida, mas para isso precisa encontrar um artefato que está na Terra.');
+    $('.item-action1').css({
+      display: 'none'
+    });
+
+  });
+});
