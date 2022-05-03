dd7eee71e7cbf50a0c356393c20032a0b06248cf
igor <85964276+igcardoso@users.noreply.github.com>
2022-05-03 12:36:59 -0300

    Added js for new movie and player pages

--- js/Index.js
+++ js/Index.js
@@ -1,3 +1,41 @@
+
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 8);
+  target.forEach(function(element) {
+    if((windowTop) > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  })
+}
+
+animeScroll();
+
+if(target.length) {
+  window.addEventListener('scroll', debounce(function() {
+    animeScroll();
+  }, 200));
+}
+
 $(document).ready(() => {
   $('').click(() => {
     $('').toggleClass('active')
@@ -35,7 +73,7 @@ $(document).ready(() => {
         items: 4
       },
       1280: {
-        items: 5
+        items: 6
       },
       1600: {
         items: 6
@@ -60,7 +98,7 @@ $(document).ready(() => {
         items: 5
       },
       1280: {
-        items: 5
+        items: 6
       },
       1600: {
         items: 6
@@ -68,6 +106,210 @@ $(document).ready(() => {
     }
   });
 });
+
+  var tt9032400 = "<iframe class='iframe" + "' src='https://drive.google.com/file/d/1Nd5RCvAsusk0reXWyDWNCqF1i4rSOy7I" + "/preview' allow='autoplay" + "'>";
+
+// BUTTON PLAY FILMS AND SERIES
+
+
+$(document).ready(function() {
+  $('.bx-button-play').click(function() {
+    
+    // BUTTON PLAY
+    
+    window.alert('Posicione o dispositivo em orientação paisagem(Deitado)')
+    
+    $('.iframe').css({
+      display: 'block'
+    });
+    
+    $('.button-back-iframe').css({
+      display: 'block',
+      zIndex: '20000'
+    });
+    
+    $('.button-lock-screen-page-movie').css({
+      display: 'inline-block'
+    });
+    
+    $('.section-info-film').css({
+      display: 'none'
+    });
+  
+    
+  });
+});
+
+// BUTTON PLAY FILMS AND SERIES
+
+// BUTTON BACK FILMS AND SERIES
+
+
+$(document).ready(function() {
+  $('.button-back-page').click(function() {
+    
+    // BUTTON BACK
+    
+    $('#page-film').css({
+      display: 'none'
+    });
+    
+    $('#menu-left').css({
+      opacity: '1',
+      zIndex: '200'
+    });
+    
+    $('#nav-bottom').css({
+      opacity: '1',
+      zIndex: '2000'
+    });
+    
+    $('.nav-wrapper').css({
+      opacity: '1',
+      zIndex: '99'
+    });
+    
+    $('.logo').css({
+      opacity: '1',
+      zIndex: '99'
+    });
+    
+    $('.hamburger-menu').css({
+      opacity: '1',
+      zIndex: '99'
+    });
+    
+    $('.background-fund').css({
+      opacity: '1'
+    });
+    
+    $('#home').css({
+      display: 'block'
+    });
+  
+    
+  });
+});
+
+// BUTTON BACK FILMS AND SERIES
+
+// BUTTON BACK IFRAME PAGE FILMS AND SERIES
+
+$(document).ready(function() {
+  $('.button-back-iframe').click(function() {
+    
+    // BUTTON BACK
+    
+    $('.section-info-film').css({
+      display: 'block'
+    });
+    
+    $('.button-back-iframe').css({
+      display: 'none'
+    });
+    
+    $('.button-lock-screen-page-movie').css({
+      display: 'none'
+    });
+    
+    $('.iframe').css({
+      display: 'none'
+    });
+    
+  });
+  
+  $('.button-back-iframe').hover(function() {
+    
+  });
+  
+  $('.iframe').hover(function() {
+ 
+  });
+  
+});
+
+// HOVER BACK IFRAME
+
+// BUTTON BACK IFRAME PAGE FILMS AND SERIES
+
+// BUTTON BACK IFRAME PAGE FILMS AND SERIES
+
+$(document).ready(function() {
+  $('.button-on-lock-screen-iframe').click(function() {
+    
+    // HOVER BUTTON LOCK SCREEN
+    
+  /*  $('.image-button-lock-screen-iframe').attr('src', 'images/icons transparent/' + 'on-lock-screen.ico');
+  */    
+    $('.gradient-lock-screen-movie-top').css({
+      display: 'block'
+    });
+    
+    $('.gradient-lock-screen-movie-bottom').css({
+      display: 'block'
+    });
+    
+    $('.button-back-iframe').css({
+      opacity: '0',
+      zIndex: '-20000'
+    });
+    
+    $('.button-off-lock-screen-iframe').css({
+      left: '8%',
+      borderTopRightRadius: '8em',
+      borderBottomRightRadius: '8em',
+      borderTopLeftRadius: '0',
+      borderBottomLeftRadius: '0',
+      paddingLeft: '2.7rem',
+      paddingRight: '0.7rem'
+    });
+    
+    $('.button-on-lock-screen-iframe').css({
+      left: '-1.1%',
+      
+    });
+    
+  });
+  
+  $('.button-off-lock-screen-iframe').click(function() {
+    
+    // HOVER BUTTON LOCK SCREEN
+    
+  /*  $('.image-button-lock-screen-iframe').attr('src', 'images/icons transparent/' + 'off-lock-screen.ico');
+  */
+    $('.gradient-lock-screen-movie-top').css({
+      display: 'none'
+    });
+    
+    $('.gradient-lock-screen-movie-bottom').css({
+      display: 'none'
+    });
+    
+    $('.button-back-iframe').css({
+      opacity: '1',
+      zIndex: '2000'
+    });
+    
+    $('.button-off-lock-screen-iframe').css({
+      left: '47%',
+      borderTopRightRadius: '0',
+      borderBottomRightRadius: '0',
+      borderTopLeftRadius: '8em',
+      borderBottomLeftRadius: '8em',
+      paddingLeft: '0.7rem',
+      paddingRight: '2rem'
+    });
+    
+    $('.button-on-lock-screen-iframe').css({
+      left: '46%'
+    });
+    
+  });
+});
+
+// HOVER BACK IFRAME
+
+// BUTTON BACK IFRAME PAGE FILMS AND SERIES
 
 // BUTTON SEARCH
 
@@ -130,12 +372,6 @@ $(document).ready(function() {
 $(document).ready(function() {
   $('.button-menu-home').click(function() {
     
-    // BACKGROUND
-    
-    $('.background-fund').css({
-      display: 'block'
-    });
-    
     // PAGES THE SITE
     
     $('#search').css({
@@ -151,6 +387,9 @@ $(document).ready(function() {
       display: 'none'
     });
     $('#film').css({
+      display: 'none'
+    });
+    $('#page-film').css({
       display: 'none'
     });
     $('#plus').css({
@@ -229,6 +468,9 @@ $(document).ready(function() {
     $('#film').css({
       display: 'none'
     });
+    $('#page-film').css({
+      display: 'none'
+    });
     $('#plus').css({
       display: 'none'
     });
@@ -302,6 +544,9 @@ $(document).ready(function() {
       display: 'block'
     });
     $('#film').css({
+      display: 'none'
+    });
+    $('#page-film').css({
       display: 'none'
     });
     $('#plus').css({
@@ -379,6 +624,9 @@ $(document).ready(function() {
     $('#film').css({
       display: 'block'
     });
+    $('#page-film').css({
+      display: 'none'
+    });
     $('#plus').css({
       display: 'none'
     });
@@ -454,6 +702,9 @@ $(document).ready(function() {
     $('#film').css({
       display: 'none'
     });
+    $('#page-film').css({
+      display: 'none'
+    });
     $('#plus').css({
       display: 'block'
     });
@@ -506,24 +757,6 @@ $(document).ready(function() {
 // ACTIONS THE BUTTONS TO PAGE HOME
 
 $(document).ready(function() {
-  $('#movie01').hover(function() {
-    $('.hero-slide-item1').css({
-      background: 'url(images/movies/theatre-dead.jpg), no-repeat center', backgroundSize: 'cover'
-    });
-    $('.item-content-title1').text('Theatre of the Dead');
-    $('.star1 span').text('6.1');
-    $('.time1 span').text('2h 29m');
-    $('.class1 span').text('16+');
-    $('.class1').css({
-      backgroundColor: '#ff0000', border: '2px solid #ff0000'
-      
-    });
-    $('.item-content-description1').text('Optimus Prime encontra seu planeta natal, Cybertron, agora um planeta morto, e descobre que foi responsável por matá-lo. Ele encontra uma maneira de trazer o planeta de volta à vida, mas para isso precisa encontrar um artefato que está na Terra.');
-    $('.item-action1').css({
-      display: 'none'
-    });
-
-  });
   $('#movie02').hover(function() {
     $('.hero-slide-item1').css({
       background: 'url(https://www.themoviedb.org/t/p/original/8yLzd8z7xRQzWG0neDrVLc34Jmx.jpg), no-repeat center', backgroundSize: 'cover'
@@ -633,3 +866,125 @@ $(document).ready(function() {
 
   });
 });
+
+// 
+
+// FILMS
+
+$(document).ready(function() {
+  // FILME tt9032400
+  $('#tt9032400').click(function() {
+    
+    // BUTTON BACK
+    
+    $('#page-film').css({
+      display: 'block'
+    });
+    
+    $('.section-info-film').css({
+      background: 'url(https://www.themoviedb.org/t/p/original/3dUByTea97X3XzznN8ZPFX9c7J7.jpg), no-repeat center', backgroundSize: 'cover'
+    });
+    
+    $('#page-film .movie-homepage-title').text('Eternos');
+    
+    $('#page-film .type').text('Ficção científica');
+    
+    $('#page-film .relevanse').text('71% Relevante');
+    
+    $('#page-film .date').text('2021');
+    
+    $('#page-film .seasons-on-minutes').text('2h 37m');
+    
+    $('#page-film .quality').text('HD');
+    
+    $('#page-film .name-the-direction').text('HD');
+    
+    $('#page-film .name-the-elenco').text('Gemma Chan, Richard Madden, Angelina Jolie, Barry Keoghan ...');
+    
+    $('#page-film .date-the-atualization').text('4 dias e 7 horas');
+    
+    $('#page-film .synopsis-of-the-film').text('Após os eventos de "Vingadores: Ultimato", os Eternos, uma raça alienígena imortal criada pelos Celestiais que vivem em segredo na Terra há mais de 7000 anos, se reúnem após uma tragédia inesperada para proteger a humanidade de seus colegas malignos: os Deviantes.');
+    
+    $('.player').append(tt9032400);
+    
+    $('#menu-left').css({
+      opacity: '0',
+      zIndex: '-2000000'
+    });
+    
+    $('.button-back-iframe').css({
+      display: 'none'
+    });
+    
+    $('#nav-bottom').css({
+      opacity: '0',
+      zIndex: '-200000'
+    });
+    
+    $('.nav-wrapper').css({
+      opacity: '0',
+      zIndex: '-2000000'
+    });
+    
+    $('.logo').css({
+      opacity: '0',
+      zIndex: '-2000000'
+    });
+    
+    $('.hamburger-menu').css({
+      opacity: '0',
+      zIndex: '-90000'
+    });
+    
+    $('.background-fund').css({
+      opacity: '0'
+    });
+    
+    $('#search').css({
+      display: 'none'
+    });
+    
+    $('#home').css({
+      display: 'none'
+    });
+    
+    $('#calender').css({
+      display: 'none'
+    });
+    
+    $('#television').css({
+      display: 'none'
+    });
+    
+    $('#film').css({
+      display: 'none'
+    });
+    
+    $('#plus').css({
+      display: 'none'
+    });
+  
+    
+  });
+  $('#tt9032400').hover(function() {
+    $('.hero-slide-item1').css({
+      background: 'url(https://www.themoviedb.org/t/p/original/3dUByTea97X3XzznN8ZPFX9c7J7.jpg), no-repeat center', backgroundSize: 'cover'
+    });
+    $('.item-content-title1').text('Eternos');
+    $('.star1 span').text('71% Relevante');
+    $('.time1 span').text('2h 37m');
+    $('.class1 span').text('12+');
+    $('.class1').css({
+      backgroundColor: '#ffa400', border: '2px solid #ffa400'
+      
+    });
+    $('.item-content-description1').text('Após os eventos de "Vingadores: Ultimato", os Eternos, uma raça alienígena imortal criada pelos Celestiais que vivem em segredo na Terra há mais de 7000 anos, se reúnem após uma tragédia inesperada para proteger a humanidade de seus colegas malignos: os Deviantes.');
+    $('.item-action1').css({
+      display: 'none'
+    });
+
+  });
+  // FILME tt9032400
+});
+
+// FILMS
